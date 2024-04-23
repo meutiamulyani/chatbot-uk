c7d8cf51262b0e30f8da85e7cd8ec2ab0d002eb7
root <root@srv512931.hstgr.cloud>
2024-04-23 09:27:45 +0000

    dimas(fix): add env configuration

--- provider/db.py
+++ provider/db.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import dotenv_values
 
-URL_DATABASE = 'postgresql://postgres:meutiaam@localhost:5432/chatbot_oohbali'
+#URL_DATABASE = 'postgresql://postgres:meutiaam@localhost:5432/chatbot_oohbali'
+
+env_vars = dotenv_values(".env")
+
+URL_DATABASE = env_vars.get('DB_URL')
+
+print("HELLO")
+
+print(URL_DATABASE)
 
 engine = create_engine(URL_DATABASE)
 
